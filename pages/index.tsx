07d3b0f5c2bdfba18b1cf1788324bdb64eabf019
Victor Busayo <ogunrinolavictor2015@gmail.com>
2025-04-07 06:25:55 +0100

    Add loading state with CircularProgress in Home comp

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import { Box, CircularProgress, Typography } from '@mui/material';
 
 export default function Home() {
   const router = useRouter();
@@ -8,5 +9,18 @@ export default function Home() {
     router.push('/insights/uri');
   }, [router]);
 
-  return <div>Uri data is loading...</div>;
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      height="100vh"
+    >
+      <CircularProgress />
+      <Typography variant="body1" mt={2}>
+        Uri data is loading...
+      </Typography>
+    </Box>
+  );
 }
